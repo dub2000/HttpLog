ae7afe5311ddd893c2cb5ce950db0301a76d2943
dub2000 <dub2000@yandex.ru>
2022-01-06 20:15:22 +0300

    [*] переименование таблицы логов в http_logs [*] добавление заголовков ответа

---
+++ src/Models/HttpLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dub2000\HttpLog\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HttpLog extends Model
+{
+    use HasFactory;
+}
