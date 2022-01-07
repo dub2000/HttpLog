11c731d38238385c43c7c4ce2ff2f35af4574603
dub2000 <dub2000@yandex.ru>
2022-01-07 16:56:36 +0300

    1

--- database/migrations/2022_01_02_173238_alter2_http_log_table.php
+++ database/migrations/2022_01_02_173238_alter2_http_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterHttpLogTable extends Migration
+class Alter2HttpLogTable extends Migration
 {
     /**
      * Run the migrations.
