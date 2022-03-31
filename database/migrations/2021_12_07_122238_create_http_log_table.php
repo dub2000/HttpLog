ff34146ce97217f02a811aaed9749069d41c4410
dub2000 <dubatov.ivan@gmail.com>
2022-03-31 16:33:29 +0300

    тестирование github

--- database/migrations/2021_12_07_122238_create_http_log_table.php
+++ database/migrations/2021_12_07_122238_create_http_log_table.php
@@ -14,6 +14,7 @@ class CreateHttpLogTable extends Migration
     public function up()
     {
         Schema::create('http_logs', function (Blueprint $table) {
+
             $table->id();
             $table->timestamps();
             $table->text('method');
